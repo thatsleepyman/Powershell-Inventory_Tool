d6b4523b3e9bbb3e75bb75dfdb35cab3197abe75
Stefan Meeuwessen <55401579+SleepyApache@users.noreply.github.com>
2022-05-16 23:57:57 +0200

    Update Inventory_Tool.ps1

--- Inventory_Tool/Inventory_Tool.ps1
+++ Inventory_Tool/Inventory_Tool.ps1
@@ -1,17 +1,17 @@
 #First run the Set-ExecutionPolicy.ps1 script if needed
 
-#Variables
+#Global Variables
 $Folder_data = $PSScriptRoot + '\Inventory_Tool_Data'
+$Timestamp = $(get-date -f yyyy-MM-dd_HH-mm-ss)
 $Folder_pcname = "$Folder_data\$env:computername"
 $Folder_timestamp = "$Folder_pcname\$Timestamp"
-
-$Timestamp = $(get-date -f yyyy-MM-dd_HH-mm-ss)
 $FileName = "$Folder_timestamp\$Timestamp"
 $Comports = (Get-WmiObject -query "SELECT * FROM Win32_PnPEntity" | Where-Object {$_.Name -Match "COM\d+"}).name
 
-$AppPath = "$FileName Applicaties.txt"
+#File specific Variables
+$AppPath = "$FileName Apps.txt"
 $DriverPath = "$FileName Drivers.txt"
-$USBPath = "$FileName USB Poorten.txt"
+$USBPath = "$FileName USB Ports.txt"
 $COMportPath = "$FileName COMports.txt"
 
 #New folders 'Directory\Inventory_Tool_Data\pcname\pcname timestamp\x.txt'
@@ -19,7 +19,7 @@ New-Item -ItemType Directory -Force -Path $Folder_data
 New-Item -ItemType Directory -Force -Path $Folder_pcname
 New-Item -ItemType Directory -Force -Path $Folder_timestamp
 
-#Applicaties
+#Apps
 #If you want .csv files instead, just # the Out-File and remove # from Export-Csv and change .txt to .csv
 New-Item -Path $AppPath -ItemType File
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |
@@ -37,7 +37,7 @@ Get-WmiObject Win32_PnPSignedDriver |
     Sort-Object -Property DeviceName, Manufacturer |
 Out-File $DriverPath
 
-#USB Poorten
+#USB Ports
 New-Item -Path $USBPath -ItemType File
 Get-PnpDevice -class USB |
      Sort-Object -Property Status, Class, FriendlyName |
@@ -50,7 +50,7 @@ if ($null -ne $Comports) {
         Out-File $COMportPath
     }
 
-#Open folder location
+#Open directory in which the files are stored
 if (-not(Get-Process explorer | Where-Object{$_.MainWindowTitle -eq ($Folder_data | Split-Path -Leaf)})) {
     explorer.exe $Folder_data
     }
